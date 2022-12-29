93bdd3af897bf9a3b5b2525cb45885b144521922
Daniel McCoy Stephenson <dmccoystephenson@gmail.com>
2022-12-29 10:06:43 -0700

    Created and used the MyLogger class.

--- src/testing.cpp
+++ src/testing.cpp
@@ -1,26 +1,12 @@
 #include <string>
 #include <iostream>
+#include "../include/myLogger.hpp"
 
-bool debugFlag = true;
-
-/**
- * Log a message to the console.
- */
-void log(std::string message) {
-    std::cout << "[LOG] " << message << std::endl;
-}
-
-/**
- * Log a debug message to the console.
- */
-void debug(std::string message) {
-    if (debugFlag) {
-        std::cout << "[DEBUG] " << message << std::endl;
-    }
-}
 
 int main() {
     std::string toPrint = "Hello World!";
-    log(toPrint);
+    
+    MyLogger logger("info.log", "error.log");
+    logger.info(toPrint);
     return 0;
 }
