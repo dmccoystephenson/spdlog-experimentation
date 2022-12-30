1f0a15c9f2816f5e15bac8d6a336e693535686b3
Daniel McCoy Stephenson <dmccoystephenson@gmail.com>
2022-12-30 12:02:59 -0700

    Utilized MyLogger class in testing.cpp.

--- src/testing.cpp
+++ src/testing.cpp
@@ -11,6 +11,15 @@ int main() {
         auto logger = spdlog::stdout_color_mt("console");
         logger->info("Welcome to spdlog!");
         logger->info("spdlog version {}  ",  SPDLOG_VERSION);
+
+        logger->info("Creating and using instance of MyLogger");
+        auto myLogger = spdlog::stdout_color_mt("MyLogger");
+        myLogger->info("MyLogger instance created");
+
+        while (true) {
+            myLogger->info("MyLogger instance is still alive");
+        }
+
     } catch (const spdlog::spdlog_ex& ex) {
         std::cout << "Log init failed: " << ex.what() << std::endl;
     }
